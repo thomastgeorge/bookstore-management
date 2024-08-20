e3d0dd3be383c578d5d83e86a67aa27a52af104f
Thomas <itsthomastgeorge@gmail.com>
2024-08-20 12:12:20 +0200

    Adding bsm-backend folder

---
+++ bsm-backend/src/main/java/com/grayMatter/BsmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grayMatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsmBackendApplication.class, args);
+	}
+
+}
---
+++ bsm-backend/src/test/java/com/grayMatter/BsmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grayMatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
