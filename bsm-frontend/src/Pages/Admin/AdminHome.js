5c9df9ff19025fb6c8dcbb28dcd2e4ace953c8f6
Thomas <itsthomastgeorge@gmail.com>
2024-09-05 22:48:02 +0200

    securtiyconfig updates

--- bsm-frontend/src/Pages/Admin/AdminHome.js
+++ bsm-frontend/src/Pages/Admin/AdminHome.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Axios from '../../Service/Axios'
 
 const AdminHome = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Define the async function to fetch data
+    const fetchData = async () => {
+      try {
+        const response = await Axios.get('/api/v1/customer/allow/102');
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    // Call the async function
+    fetchData();
+  }, []); // Empty dependency array means this useEffect runs once after the initial render
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div>
-      AdminHome
+      <h1>Admin Home</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
-}
+  );
+};
 
 export default AdminHome;
+
